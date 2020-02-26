5491eea1c19bc2b7f49e725d2a0a3e655de48c5f
MarcusCYH <marcus.chan@flexi2pay.com>
2020-02-26 10:42:19 +0800

    edit txt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Test 2
+                    Laravel Branch test
                 </div>
 
                 <div class="links">
